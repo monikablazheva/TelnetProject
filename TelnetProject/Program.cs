7727b3a8ecb308d354fe97e12e070a80698c1ce1
monikablazheva <monikablazheva_e18@schoolmath.eu>
2022-11-14 16:09:33 +0200

    Add project files.

---
+++ TelnetProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TelnetProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = 4096;
+            int portNum = 8023;
+
+            var tcpListener = new TcpListener(IPAddress.Any, portNum);
+            tcpListener.Start();
+            Console.WriteLine("Listening on port {0}...", portNum);
+
+            byte[] buffer = new byte[lenght];
+            using var stream = tcpListener.AcceptTcpClient().GetStream();
+            while (true)
+            {
+                SendToUser("Welcome to Telnet Server!");
+                int streamLenght = stream.Read(buffer);
+                var command = Encoding.UTF8.GetString(buffer, 0, streamLenght);
+                //Process command
+                //Response to user
+            }
+
+            void SendToUser(string message)
+            {
+                var buffer = Encoding.UTF8.GetBytes(message);
+                stream.Write(buffer, 0, message.Length);
+            }
+        }
+    }
+}
